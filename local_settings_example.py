684a69e4004976791f1b98c06ba783b47e53c658
Ross Gardler <rgardler@apache.org>
2010-12-11 03:05:20 +0000

    add an example local_settings file

---
+++ local_settings_example.py
@@ -0,0 +1,13 @@
+# Details of email account through which to send emails
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'host.address.org'
+EMAIL_HOST_USER = 'username'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+
+# Details of a POP3 account to retrieve emails from
+# This account should be subscribed to any mailing lists you wish to track
+SUBSCRIPTION_POP3_SERVER = "pop3.server.org"
+SUBSCRIPTION_POP3_USER = "username"
+SUBSCRIPTION_POP3_PASSWORD = "password"
+SUBSCRIPTION_POP3_DELETE = False # set to true if you want listeningPost to delete emails
